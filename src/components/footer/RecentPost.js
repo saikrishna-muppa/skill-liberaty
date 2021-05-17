2349e308af4060949e3e76190eadea678d75eb37
saikrishna-muppa <muppasaikrishna@gmail.com>
2021-05-17 12:15:53 +0530

    'addedrecentpost'

--- src/components/footer/RecentPost.js
+++ src/components/footer/RecentPost.js
@@ -6,7 +6,7 @@ function RecentPost() {
             <div className="recent-post-card">
                 
                     <img src={icon} alt="pic"/>
-                                <div className="card-text">
+                                <div className="card-text" style={{color:"white"}}>
                    How to get Programming Language Cartification in 45 days
                 </div>
             </div>
