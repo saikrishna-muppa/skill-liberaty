56bbedd4994018a423856f934b9db0561e10198b
saikrishna-muppa <muppasaikrishna@gmail.com>
2021-05-19 14:01:15 +0530

    modified about

--- src/components/about/homepage/About.js
+++ src/components/about/homepage/About.js
@@ -1,112 +1,118 @@
 import React from "react";
 import { Card, Avatar } from "antd";
-import '../homepage/About.css'
+import "../homepage/About.css";
 import ScoreBlock from "../../scoreBlock/ScoreBlock";
 import CourseCards from "../../course/cards/CourseCards";
 
-import { Pagination } from 'antd';
+import { Pagination } from "antd";
 import ContactUs from "../contactUs/ContactUs";
 import TeachersCards from "../teachersCards/TeachersCards";
 const { Meta } = Card;
 const About = () => {
-
-
   return (
-      <div>
-    <div className="about-container">
-      <div className="about-header">
-        <h3>Welcome to the Professional Education Theme!</h3>
-        <Card
-          className="about-card"
-          style={{ width: 300 }}
-          cover={
-            <img
-              className="about-avatar"
-              alt="example"
-              src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+    <div>
+      <div className="about-container">
+        <div className="about-header">
+          <h3>Welcome to the Professional Education Theme!</h3>
+          <Card
+            className="about-card"
+            style={{ width: 300 }}
+            cover={
+              <img
+                className="about-avatar"
+                alt="example"
+                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+              />
+            }
+          >
+            <Meta
+              title="Group Seminars"
+              description="consectetur adipisicing elit, sed do eiusmod tempor"
             />
-          }
-        >
-          <Meta
-            title="Group Seminars"
-            description="consectetur adipisicing elit, sed do eiusmod tempor"
-          />
-        </Card>
-        <Card
-          className="about-card"
-          style={{ width: 300 }}
-          cover={
-            <img
-              className="about-avatar"
-              alt="example"
-              src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+          </Card>
+          <Card
+            className="about-card"
+            style={{ width: 300 }}
+            cover={
+              <img
+                className="about-avatar"
+                alt="example"
+                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+              />
+            }
+          >
+            <Meta
+              title="Expert Teachers"
+              description="consectetur adipisicing elit, sed do eiusmod tempor"
             />
-          }
-        >
-          <Meta
-            title="Expert Teachers"
-            description="consectetur adipisicing elit, sed do eiusmod tempor"
-          />
-        </Card>
-        <Card
-          className="about-card"
-          style={{ width: 300 }}
-          cover={
-            <img
-              className="about-avatar"
-              alt="example"
-              src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+          </Card>
+          <Card
+            className="about-card"
+            style={{ width: 300 }}
+            cover={
+              <img
+                className="about-avatar"
+                alt="example"
+                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+              />
+            }
+          >
+            <Meta
+              title="Large Library"
+              description="consectetur adipisicing elit, sed do eiusmod tempor"
             />
-          }
-        >
-          <Meta
-            title="Large Library"
-            description="consectetur adipisicing elit, sed do eiusmod tempor"
-          />
-        </Card>
-        <Card
-          className="about-card"
-          style={{ width: 300 }}
-          cover={
-            <img
-              className="about-avatar"
-              alt="example"
-              src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+          </Card>
+          <Card
+            className="about-card"
+            style={{ width: 300 }}
+            cover={
+              <img
+                className="about-avatar"
+                alt="example"
+                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+              />
+            }
+          >
+            <Meta
+              title="Trending Courses"
+              description="consectetur adipisicing elit, sed do eiusmod tempor"
             />
-          }
-        >
-          <Meta
-            title="Trending Courses"
-            description="consectetur adipisicing elit, sed do eiusmod tempor"
-          />
-        </Card>
-        
+          </Card>
         </div>
         <div>
-        <img className="about-side-image" alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
+          <img
+            className="about-side-image"
+            alt="example"
+            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+          />
         </div>
-        </div>
-        <div className="about-score-block">
-        <ScoreBlock/>
-        </div>
-        <h2 className="about-cards-text">Various courses to choose from</h2>
-        <CourseCards/>
+      </div>
+      <div className="about-score-block">
+        <ScoreBlock />
+      </div>
+      <h2 className="about-cards-text">Various courses to choose from</h2>
+      <CourseCards />
+      <div className="contactus">
         
-        <ContactUs/>
-<div className="teacherscards">   
-       <TeachersCards />
-</div>
+        <ContactUs />
+      </div>
 
-  <div className="about-join">
-    <h3>Want to Join?</h3>
-    <p>Lorem ipsum dolor sit amet elit. Nisi facere reprehenderit!</p>
-    <div class="link-list-menu">
-           
-            <a href="about.html" class="btn-join-about-primary">Become Teacher </a>
-            <a href="about.html" class="btn-join-about">Become Teacher </a>
+      <div className="teacherscards">
+        <TeachersCards />
+      </div>
+
+      <div className="about-join">
+        <h3>Want to Join?</h3>
+        <p>Lorem ipsum dolor sit amet elit. Nisi facere reprehenderit!</p>
+        <div class="link-list-menu">
+          <a href="about.html" class="btn-join-about-primary">
+            Become Teacher
+          </a>
+          <a href="about.html" class="btn-join-about">
+           Signup
+          </a>
         </div>
-  </div>
-
+      </div>
     </div>
   );
 };
